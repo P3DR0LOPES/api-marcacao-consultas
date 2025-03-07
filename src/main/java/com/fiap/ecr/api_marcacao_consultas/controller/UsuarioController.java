6abe7123db4d421f3b6b666f621c8416e98f7990
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-07 20:50:19 -0300

    Configuração dos Pacotes

---
+++ src/main/java/com/fiap/ecr/api_marcacao_consultas/controller/UsuarioController.java
@@ -0,0 +1,2 @@
+package com.fiap.ecr.api_marcacao_consultas.Controller;public class UsuarioController {
+}
---
+++ src/main/java/com/fiap/ecr/api_marcacao_consultas/model/Especialidade.java
@@ -0,0 +1,2 @@
+package com.fiap.ecr.api_marcacao_consultas.Model;public class Especialidade {
+}
